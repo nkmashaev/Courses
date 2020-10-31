1649ca6fada0aa36f41a9eca1d02a20f4be06c26
n.mashaev <nikitamashaev@gmail.com>
2020-11-01 02:15:12 +0300

    MR!10(#5) [HW1 Task04] Find number of tuples

---
+++ homework1/task04/task04.py
@@ -0,0 +1,35 @@
+"""
+Implemention of fourth task of hw1
+Given four lists A, B, C, D of integer values,
+    compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+We guarantee, that all A, B, C, D have same length of N where 0 ≤ N ≤ 1000.
+"""
+
+from typing import List
+
+
+def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) -> int:
+    """
+    Function check_sum_of_four calculates ow many tuples (i, j, k, l) there are such
+    that A[i] + B[j] + C[k] + D[l] is zero
+
+    :param a: first list of integer values
+    :type a: List[int]
+    :param b: second list of integer values
+    :type b: List[int]
+    :param c: third list of integer values
+    :type c: List[int]
+    :param d: fourth list of integer values
+    :type d: List[int]
+    :return: number of tuples (i, j, k, l) for which A[i] + B[j] + C[k] + D[l] condition is true
+    :rtype: int
+    """
+    counter = 0
+
+    for a_i in a:
+        for b_j in b:
+            for c_k in c:
+                for d_l in d:
+                    if a_i + b_j + c_k + d_l == 0:
+                        counter += 1
+    return counter
