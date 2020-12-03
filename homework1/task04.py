4821424e0144f60cce9d7af89dd7753e50eb26fc
n.mashaev <nikitamashaev@gmail.com>
2020-12-03 23:36:29 +0300

    MR!29(#28) [FIX HW1] Task04, Task05 speedup

--- homework1/task04.py
+++ homework1/task04.py
@@ -5,13 +5,16 @@ Given four lists A, B, C, D of integer values,
 We guarantee, that all A, B, C, D have same length of N where 0 ≤ N ≤ 1000.
 """
 
+from collections import defaultdict
 from typing import List
 
 
-def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) -> int:
+def check_sum_of_enumeration(
+    a: List[int], b: List[int], c: List[int], d: List[int]
+) -> int:
     """
-    Function check_sum_of_four calculates ow many tuples (i, j, k, l) there are such
-    that A[i] + B[j] + C[k] + D[l] is zero
+    Function check_sum_of_four calculates how many tuples (i, j, k, l) there are such
+    that A[i] + B[j] + C[k] + D[l] is zero. O(n^4)
 
     :param a: first list of integer values
     :param b: second list of integer values
@@ -27,4 +30,31 @@ def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) ->
                 for d_l in d:
                     if a_i + b_j + c_k + d_l == 0:
                         counter += 1
+    return counter
+
+
+def check_sum_of_four_dict(
+    a: List[int], b: List[int], c: List[int], d: List[int]
+) -> int:
+    """
+    Function check_sum_of_four calculates how many tuples (i, j, k, l) there are such
+    that A[i] + B[j] + C[k] + D[l] is zero. O(n^2)
+
+    :param a: first list of integer values
+    :param b: second list of integer values
+    :param c: third list of integer values
+    :param d: fourth list of integer values
+    :return: number of tuples (i, j, k, l) for which A[i] + B[j] + C[k] + D[l] condition is true
+    """
+
+    sum_dict = defaultdict(int)
+    for a_i in a:
+        for b_j in b:
+            sum_dict[a_i + b_j] += 1
+
+    counter = 0
+    for c_k in c:
+        for d_l in d:
+            counter += sum_dict[-(c_k + d_l)]
+
     return counter
