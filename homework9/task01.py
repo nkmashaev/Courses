02a65d2ea940690ef2d17ac9020b9f4fc9f94503
nkmashaev <nikita_mashaev@mail.ru>
2020-12-30 05:41:53 +0300

    MR!49(#50) [HW9 FIX] hw9 fixes

--- homework9/task01.py
+++ homework9/task01.py
@@ -5,23 +5,8 @@ from typing import Iterator, List, Union
 
 
 def get_data(file_path: Union[Path, str]) -> Iterator[int]:
-    if isinstance(file_path, Path):
-        if not file_path.exists():
-            raise AttributeError("Error: Path not exists")
-        if not file_path.is_file():
-            raise AttributeError("Error: Expected file name")
-    if isinstance(file_path, str):
-        if not os.path.exists(file_path):
-            raise AttributeError("Error: Path not exists")
-        if not os.path.isfile(file_path):
-            raise AttributeError("Error: Expected file name")
-    else:
-        raise AttributeError("Error: Expected str or Path")
     with open(file_path, "r") as in_file:
-        while True:
-            data = in_file.readline()
-            if not data:
-                break
+        for data in in_file:
             yield int(data)
 
 
